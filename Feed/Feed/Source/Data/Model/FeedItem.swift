9cb1eabdb7b1fbfb026d215e858f02400a0d41fe
Andre Luis Campopiano <t3656372@timbrasil.com.br>
2023-05-03 16:17:00 -0300

    Create model FeedItem

---
+++ Feed/Feed/Source/Data/Model/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  Feed
+//
+//  Created by Andre Luis Campopiano on 03/05/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
